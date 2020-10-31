f7a53e999f4de826e5753f56839f0c4594af585e
mturic <mturic@inovex.de>
2020-10-31 13:20:22 +0100

    new: added usage of material-angular/table to display data

--- repair4youFrontend/src/app/app.module.ts
+++ repair4youFrontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { MaintenanceOverviewComponent } from './maintenance-overview/maintenance-overview.component';
 import { MaintenanceMeetingComponent } from './maintenance-meeting/maintenance-meeting.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 const appRoutes: Routes = [
   { path: 'overview', component: MaintenanceOverviewComponent },
   { path: 'newMeeting', component: MaintenanceMeetingComponent },
-  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -28,7 +30,9 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- repair4youFrontend/src/app/maintenance-overview/maintenance-overview.component.ts
+++ repair4youFrontend/src/app/maintenance-overview/maintenance-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-maintenance-overview',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maintenance-overview.component.css']
 })
 export class MaintenanceOverviewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  /*
+  
+  id: Numeric (Mandatory),
+  vehicleBrand: String(Mandatory),
+  vehicleLicencePlate: String(Mandatory),
+  maintenanceDate: Date (Mandatory)
+  vehicleContactPerson: String,
+  vehicleContactNumber: String
+  
+  */
+
+  data = [];
+  displayColumns: string[] = [
+    'ID',
+    'vehicleBrand',
+    'vehicleLicencePlate',
+    'maintenanceDate',
+    'vehicleContactPerson',
+    'vehicleContactNumber'
+  ];
+  dataSource = new MatTableDataSource<any>();
 
   ngOnInit(): void {
+    this.data = this.getData();
+    this.dataSource.data = this.data;
   }
 
+  private getData(): any {
+    return [
+      {
+        year: 1991,
+        movie: 'Critters 3',
+        role: 'Josh',
+        director: 'Kristine Peterson',
+        notes: 'Direct-to-video'
+      },
+      {
+        year: 1992,
+        movie: 'Poison Ivy',
+        role: 'Guy',
+        director: 'Katt Shea',
+        notes: 'Credited as Leonardo Di Caprio'
+      },
+      {
+        year: 1993,
+        movie: "This Boy's Life",
+        role: 'Tobias Toby Wolff',
+        director: 'Michael Caton-Jones'
+      }
+    ];
+  }
 }
